654063ad64922ad74670447ea8b89e5bba936832
Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>
2023-04-22 00:51:59 -0300

    fix: remove Windows & Linux; add: fonts

---
+++ lib/providers/account.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:appwrite/appwrite.dart' as appwrite;
+import 'package:readqr/api/client.dart';
+import 'package:appwrite/models.dart';
+import 'package:flutter/material.dart';
+import 'package:readqr/data/store.dart';
+
+class AccountProvider extends ChangeNotifier {
+  User? _current;
+  User? get current => _current;
+
+  Session? _session;
+  Session? get session => _session;
+
+  Future<Session?> get _cachedSession async {
+    final cached = await Store.get("session");
+
+    if (cached == null) {
+      return null;
+    }
+
+    return Session.fromMap(json.decode(cached));
+  }
+
+  Future<bool> isValid() async {
+    if (session == null) {
+      final cached = await _cachedSession;
+
+      if (cached == null) {
+        return false;
+      }
+
+      _session = cached;
+    }
+
+    return _session != null;
+  }
+
+  Future<void> register(String email, String password, String? name) async {
+    try {
+      final result = await ApiClient.account.create(
+          userId: appwrite.ID.unique(),
+          email: email,
+          password: password,
+          name: name);
+
+      _current = result;
+
+      notifyListeners();
+    } catch (e) {
+      throw Exception("Failed to register");
+    }
+  }
+
+  Future<void> login(String email, String password) async {
+    try {
+      final result = await ApiClient.account.createEmailSession(
+        email: email,
+        password: password,
+      );
+      _session = result;
+
+      Store.set("session", json.encode(result.toMap()));
+
+      notifyListeners();
+    } catch (e) {
+      _session = null;
+    }
+  }
+}
